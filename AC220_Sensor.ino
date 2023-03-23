efd2d6a2bcbe9aae47eb1af83af71ff077a49b2c
JAE-HONG, LEE <90753251+JaeHongLeee@users.noreply.github.com>
2023-03-23 18:17:59 +0900

    Add files via upload

--- AC220_Sensor.ino
+++ AC220_Sensor.ino
@@ -11,18 +11,16 @@ portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
 static long analogPinTimer = 0;
 unsigned long thisMillis_old;
-int sensorValue;
-
-int swtich_number;  // '1': Data collection, '2': Data Calculation
+int sensorValue_1, sensorValue_2;
 
 unsigned long pastMillis_0 = 0; 
 unsigned long pastMillis_1 = 0;
 unsigned long pastMillis_2 = 0;
 int timerflag = 0;  // 초기 상태
 
-int i = 0;  // 데이터 구별 숫자
+int i, j = 0;  // 데이터 구별 숫자
 int Data_arr1[500];
-int Data_num1;
+int Data_arr2[500];
 
 void IRAM_ATTR onTimer_1() { // interrupt function
   portENTER_CRITICAL_ISR(&timerMux);
@@ -38,6 +36,7 @@ void setup() {
   timerAttachInterrupt(timer, &onTimer_1, true);
   timerAlarmWrite(timer, 1000000*60*5, true); // run for 5 minutes
   timerAlarmEnable(timer);
+
 }
 
 void loop() {
@@ -67,19 +66,28 @@ void loop() {
 
       if(analogPinTimer <= 0) {
         analogPinTimer += ANALOG_PIN_TIMER_INTERVAL;
-        sensorValue = analogRead(CHANNEL_1);
+        sensorValue_1 = analogRead(CHANNEL_1);
+        sensorValue_2 = analogRead(CHANNEL_2);
 
-        Data_arr1[i] = sensorValue;
+        Data_arr1[i] = sensorValue_1;
+        Data_arr2[j] = sensorValue_2;
             
         Serial.print(thisMillis/1000.0);  // Sampleing Time 1ms setting
         Serial.print(",");
-        Serial.print(sensorValue);
+        Serial.print(sensorValue_1);
+        Serial.print(",");
+        Serial.print(sensorValue_2);
         Serial.print(",");
         Serial.print(Data_arr1[i]);
         Serial.print(",");
-        Serial.println(i);
-
+        Serial.print(i);
+        Serial.print(",");
+        Serial.print(Data_arr2[j]);
+        Serial.print(",");
+        Serial.println(j);
+      
         i++;
+        j++;
 
         if((thisMillis%300000) == 0){ // Operations per 5 minutes
           Serial.print("5555555555555555555555555555555555555");
@@ -88,30 +96,38 @@ void loop() {
     break;
 
     case 1: // timerflag = 1, 9.5 초간 수행 할 작업 실행
-      Serial.println("Data Calculation Start");
-
-
-
-      timerflag = 0;
-      break;
-
-    case 2: // timerflag = 2
+      //Serial.println("Data Calculation Start");
+      if(thisMillis - pastMillis_1 > 9500){
+        timerflag = 0;
+        Serial.println("Set Position");
+      }
       break;
  }
 
-  if(thisMillis - pastMillis_0 >= 3000){  // 3초가 지났을때 timerflag = 1 로 변경
+  if(thisMillis - pastMillis_0 >= 4000){  // 4초가 지났을때 timerflag = 1 로 변경
     pastMillis_0 = thisMillis;
-    Serial.print("on");
-    Serial.print(",");
-    Serial.println(i);
     timerflag = 1;
+    Serial.println("Data Calculation Start");
+    
   }
-   
+
+  /*
+  if(timerflag == 1){
+    if(thisMillis - pastMillis_1 >= 9500){
+      pastMillis_1 = thisMillis;
+      Serial.println("Data Calculation End");
+      digitalWrite(2, !(digitalRead(2)));
+      delay(1000);
+      timerflag = 0;
+    }
+  }
+  /*
   if(thisMillis - pastMillis_1 >= 9500 && timerflag == 1){ // 9.5초가 지나고 timerflag = 1 일때, timerflag = 0 로 변경
     pastMillis_1 = thisMillis;
     digitalWrite(2, !(digitalRead(2)));
     delay(1000);
     timerflag = 0;
   }
+  */
  
 }
