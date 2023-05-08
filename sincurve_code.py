037824755a2f75a82d99fc8d1e4ee575dc65016a
Jae-Hong, Lee <90753251+JaeHongLeee@users.noreply.github.com>
2023-05-08 19:36:19 +0900

    Add files via upload

--- sincurve_code.py
+++ sincurve_code.py
@@ -3,26 +3,36 @@ from math import pi
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("1_(0A, 0A).txt")	#insert ffile name
-y = data
+data = np.loadtxt("20230508171235.txt")	#insert file name
+y1 = data[:, 0]
+y2 = data[:, 1]
 
 def sin_func(x, A, f, phi, d):
 	return A * np.sin(2* np.pi * f * x + phi) + d
 	
 initial_guess = [1, 60*2*pi, 0, 0]	#declear parmeater
 
-x = np.arange(len(y))
+params1, _ = curve_fit(sin_func, np.arange(len(y1)), y1, p0 = initial_guess)
+params2, _ = curve_fit(sin_func, np.arange(len(y2)), y2, p0 = initial_guess)
+
+x = np.linspace(0, len(y1)-1, len(y1)*10)
+
+#x = np.arange(len(y))
 	
-popt, pcov = curve_fit(sin_func, x, y, p0=initial_guess)
+#popt, pcov = curve_fit(sin_func, x, y, p0=initial_guess)
 
 
-print("amplitude A = ", popt[0])
-print("frequency f = ", popt[1])
-print("phase phi = ", popt[2])
-print("offset d = ", popt[3])
+#print("amplitude A = ", popt[0])
+#print("frequency f = ", popt[1])
+#print("phase phi = ", popt[2])
+#print("offset d = ", popt[3])
 
 
 #plt.plot(x, y, 'b.', label = 'data')
-plt.plot(x, sin_func(x, *popt), 'r-', label='Fit')
+#plt.plot(x, sin_func(x, *popt), 'r-', label='Fit')
+#plt.plot(np.arange(len(y1)), y1, label='y1 data')
+plt.plot(x, sin_func(x, *params1), label= 'y1 fit')
+#plt.plot(np.arange(len(y2)), y2, label='y2 data')
+plt.plot(x, sin_func(x, *params2), label= 'y2 fit')
 plt.legend()
 plt.show()
