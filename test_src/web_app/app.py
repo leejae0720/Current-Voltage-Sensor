5c6327b29551fb64c8ffdb44c8db4078cd0f73fb
Jae-Hong, Lee <90753251+JaeHongLeee@users.noreply.github.com>
2023-08-11 15:43:49 +0900

    Add files via upload

--- test_src/web_app/app.py
+++ test_src/web_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template, send_from_directory, request, Response
 import os
 from flask_httpauth import HTTPBasicAuth
 
@@ -6,13 +6,13 @@ app = Flask(__name__)
 auth = HTTPBasicAuth()
 
 users = {
-	"mirae": "mirae" # revise your ID and password
+    "mirae": "mirae"  # revise your ID and password
 }
 
 @auth.verify_password
 def verify_password(username, password):
-	if username in users and password == users[username]:
-		return username
+    if username in users and password == users[username]:
+        return username
 
 @app.route('/')
 def hello():
@@ -21,8 +21,9 @@ def hello():
 @app.route('/view_csv')
 @auth.login_required
 def view_csv():
-    csv_filename = '20230809_1500.csv'	# insert file name
-    csv_path = os.path.join(app.root_path, csv_filename)
+    csv_filename = 'Mirae Power Data.csv'  # insert file name
+    csv_directory = "/home/irms/web_app/Power_data"
+    csv_path = os.path.join(csv_directory, csv_filename)
 
     if os.path.exists(csv_path):
         with open(csv_path, 'r') as file:
@@ -34,9 +35,16 @@ def view_csv():
 @app.route('/download_csv')
 @auth.login_required
 def download_csv():
-    csv_filename = '20230809_1500.csv'
-    csv_path = os.path.join(app.root_path, csv_filename)
-    return send_from_directory(directory=app.root_path, filename=csv_filename, as_attachment=True)
+    csv_filename = 'Mirae Power Data.csv'
+    csv_directory = "/home/irms/web_app/Power_data"
+    csv_path = os.path.join(csv_directory, csv_filename)
+    return send_from_directory(directory=csv_directory, filename=csv_filename, as_attachment=True)
+
+# Set Cache-Control header to prevent caching
+@app.after_request
+def add_cache_control(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, proxy-revalidate'
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
